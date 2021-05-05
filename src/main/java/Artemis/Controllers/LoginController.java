c68f27a1f1d351a8d554adc1bca08515a98cde37
Ev1dentSnow <53832997+Ev1dentSnow@users.noreply.github.com>
2021-05-05 16:30:28 +0200

    Code cleanup + added data validation to prevent needless HTTP requests

--- src/main/java/Artemis/Controllers/LoginController.java
+++ src/main/java/Artemis/Controllers/LoginController.java
@@ -34,17 +34,12 @@ import java.util.List;
 
 public class LoginController extends Application {
 
-
     @FXML
     TextField txfUsername = new TextField();
 
     @FXML
     PasswordField txfPassword = new PasswordField();
 
-    String[] userTypes = {"Student", "Teacher", "Administrator"};
-
-    @FXML
-    ComboBox choiceBox = new ComboBox(FXCollections.observableArrayList(userTypes));
 
     private String accessToken = "";
     private int permissionLevel = 0;
@@ -65,75 +60,68 @@ public class LoginController extends Application {
         String username = txfUsername.getText();
         String password = txfPassword.getText();
 
-        int responseStatusCode = 0;
+        if(!username.equals("") && !password.equals("")) {
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://127.0.0.1:5000/auth/login");
+            int responseStatusCode = 0;
 
-        List<NameValuePair> body = new ArrayList<NameValuePair>();
-        body.add(new BasicNameValuePair("username", username));
-        body.add(new BasicNameValuePair("password", password));
-        httpPost.setEntity(new UrlEncodedFormEntity(body));
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost("http://127.0.0.1:5000/auth/login");
 
-
-        try{
-            CloseableHttpResponse response = httpClient.execute(httpPost);
-            responseStatusCode = response.getStatusLine().getStatusCode();
-            String responseBody = EntityUtils.toString(response.getEntity());
-            JSONObject tokenobj = new JSONObject(responseBody);
-            String responseToken = (String) tokenobj.get("access_token");
-            int permissionLevel = (Integer) tokenobj.get("role");
-            int userId = (Integer) tokenobj.get("user_id");
+            List<NameValuePair> body = new ArrayList<NameValuePair>();
+            body.add(new BasicNameValuePair("username", username));
+            body.add(new BasicNameValuePair("password", password));
+            httpPost.setEntity(new UrlEncodedFormEntity(body));
 
 
-            this.setAccessToken(responseToken);
-            this.setPermissionLevel(permissionLevel);
-            StudentDashboard.setUserId(userId);
-            StudentDashboard.setAccessToken(responseToken);
-        }
-        catch(ConnectException e){
-
-            Alert connError = new Alert(Alert.AlertType.ERROR);
-            connError.setContentText("Error connecting to server");
-            connError.showAndWait();
-        }
+            try {
+                CloseableHttpResponse response = httpClient.execute(httpPost);
+                responseStatusCode = response.getStatusLine().getStatusCode();
+                String responseBody = EntityUtils.toString(response.getEntity());
 
 
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
 
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                if (responseStatusCode == 200) {
+                    alert.setContentText("Login Successful");
+                    alert.showAndWait();
 
-        if(responseStatusCode == 200){
-            alert.setContentText("Login Successful");
-            alert.showAndWait();
+                    JSONObject tokenobj = new JSONObject(responseBody);
+                    String responseToken = (String) tokenobj.get("access_token");
+                    int permissionLevel = (Integer) tokenobj.get("role");
+                    int userId = (Integer) tokenobj.get("user_id");
 
-            AnchorPane StudentDashboard = (AnchorPane) FXMLLoader.load(getClass().getResource("/StudentDashboard.fxml"));
-            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-            window.setScene(new Scene(StudentDashboard));
-            StudentDashboard.getStylesheets().setAll(BootstrapFX.bootstrapFXStylesheet());
-            window.setResizable(false);
-            window.setWidth(1100);
-            window.setHeight(680);
-            window.show();
+                    this.setAccessToken(responseToken);
+                    this.setPermissionLevel(permissionLevel);
+                    StudentDashboard.setUserId(userId);
+                    StudentDashboard.setAccessToken(responseToken);
 
+                    AnchorPane StudentDashboard = (AnchorPane) FXMLLoader.load(getClass().getResource("/StudentDashboard.fxml"));
+                    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    window.setScene(new Scene(StudentDashboard));
+                    StudentDashboard.getStylesheets().add("TabbedPanes.css");
+                    window.setResizable(false);
+                    window.setWidth(1100);
+                    window.setHeight(680);
+                    window.show();
+                } else {
 
+                    alert.setContentText("Login Failed, HTTP STATUS CODE: " + responseStatusCode);
+                    alert.showAndWait();
+
+                }
+
+            } catch (ConnectException e) {
+
+                Alert connError = new Alert(Alert.AlertType.ERROR);
+                connError.setContentText("Error connecting to server");
+                connError.showAndWait();
+            }
         }
         else{
-
-
-            alert.setContentText("Login Failed, HTTP STATUS CODE: " + responseStatusCode);
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Please enter both a username and a password");
             alert.showAndWait();
-
-            AnchorPane StudentDashboard = (AnchorPane) FXMLLoader.load(getClass().getResource("/StudentDashboard.fxml"));
-            StudentDashboard.getStylesheets().add("TabbedPanes.css");
-            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-            window.setScene(new Scene(StudentDashboard));
-            window.setResizable(false);
-            window.show();
-
-
-
         }
-
 
     }
 
