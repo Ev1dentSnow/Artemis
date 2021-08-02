e94568ac480ff6f59bdbfe914d1bb8b5aea78346
Ev1dentSnow <moagi.m.moja@gmail.com>
2021-08-02 22:53:31 +0200

    WIP Converting project to be modular

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module Artemis{
+
+    requires javafx.fxml;
+    requires com.google.gson;
+    requires com.auth0.jwt;
+    requires org.apache.httpcomponents.httpcore;
+    requires json;
+    requires com.calendarfx.view;
+    requires org.apache.httpcomponents.httpclient;
+}
